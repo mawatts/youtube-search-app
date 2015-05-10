774b15f5e75cf529e8d21d878c2e2cca382a46f7
mawatts <michaelwatts80@yahoo.com>
2015-05-09 23:37:28 -0400

    YouTube Search functionality with API use

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
 $(function() {
- 
+ $('#search-term').submit(function(event) {
+  event.preventDefault();
+  var searchTerm = $('#query').val();
+  getRequest(searchTerm);
+ });
+});
+
+var gapiKey = "AIzaSyCgjn-j_1L8qw0RAiMLJahj2i2cBbsVhC8";
+
+function getRequest(searchTerm) {
+ var params = {
+  part : 'snippet',
+  q : searchTerm,
+  key : gapiKey
+ };
+ url = 'https://www.googleapis.com/youtube/v3/search';
+
+ $.getJSON(url, params, function(data) {
+  showResults(data.items);
+ });
 }
+
+function showResults(results) {
+ var html = "";
+ $.each(results, function(index,value) {
+  html += '<br><a href="https://www.youtube.com/watch?v=' + value.id.videoId + '"><img src="' + value.snippet.thumbnails.medium.url + '"></a><br>';
+  console.log(value.snippet.thumbnails.medium.url);
+  console.log(value.id.videoId);
+ });
+
+$('#search-results').html(html);
+}
+
